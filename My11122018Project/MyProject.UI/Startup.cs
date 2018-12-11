db58fa9637b950dd78bd7fc34933cdc7d1c7734a
jtaptso <taptso@yahoo.fr>
2018-12-11 04:13:28 +0100

    Initial solution setup

---
+++ My11122018Project/MyProject.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyProject.UI.Startup))]
+namespace MyProject.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
